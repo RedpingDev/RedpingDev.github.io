7781b28e7c813c9231b3c57758443ab5138167e9
redping32@gmail.com <redping32@gmail.com>
2026-03-01 10:52:48 +0900

    feat: redesign homepage with dark indie game dev theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Redping Indie Game Dev';
+export const SITE_DESCRIPTION = 'Devlog of an indie game developer building co-op experiences with Godot 4. Documenting the journey, one commit at a time.';
